591918f7ae6b0a6252c86d161a1cf5bde1a641d7
yudetamago <yudetamago.orz@gmail.com>
2021-08-28 02:23:50 +0900

    fix

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,7 +5,7 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() public ERC20("MyToken", "MT") {
+    constructor() ERC20("MyToken", "MT") {
         console.log("deploy my token");
         _mint(msg.sender, 1000);
     }
